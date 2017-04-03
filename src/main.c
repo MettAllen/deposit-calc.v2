41fd90ffb30c78cad1e9b4a014d242cef34aba51
Vladislav2104 <mer.vlad@mail.ru>
2017-04-03 19:09:26 +0600

    Create dir src,bin,build

---
+++ src/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int Chek(int v, int s) {
+    if (v < 10000) {
+        printf("\n ne verno ukazan deposit \n");
+        return 1;
+    }
+    if (s <= 0 || s > 365) {
+        printf("\n ne verno ukazan period \n");
+        return 1;
+    }
+    return 0;
+};
+
+void Dohod(int v, int s) {
+    if (v > 100000) {
+        if (s <= 30)
+            v = v - v / 10;
+        if (s > 30 && s <= 120)
+            v = 1.03 * v;
+        if (s > 120 && s <= 240)
+            v = 1.08 * v;
+        if (s > 240 && s <= 365)
+            v = 1.15 * v;
+    }
+
+    if (v <= 100000) {
+        if (s <= 30)
+            v = v - v / 10;
+        if (s > 30 && s <= 120)
+            v = 1.02 * v;
+        if (s > 120 && s <= 240)
+            v = 1.06 * v;
+        if (s > 240 && s <= 365)
+            v = 1.12 * v;
+    }
+
+    printf("\ndohod=%d\n", v);
+
+};
+int main() {
+    int a, b;
+
+    printf("vvedite rasmer vklada i srok\n");
+    do {
+        scanf("%d %d", &b, &a);
+    } while (Chek(b, a));
+
+    printf("v:%d s:%d\n", b, a);
+
+    Dohod(b, a);
+
+    return 0;
+}
