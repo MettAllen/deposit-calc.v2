ba7b90b52acf159b9fe50daf7ed0b58fbd972add
Vladislav2104 <mer.vlad@mail.ru>
2017-04-22 15:20:37 +0600

    fix deposit-tets.c file4

--- test/deposit-test.c
+++ test/deposit-test.c
@@ -13,104 +13,104 @@ CTEST(calc_test, calc)
 {
     int result = Dohod(123400,140);
     
-    const int expected = 133272;
+    const void expected = 133272;
     ASSERT_EQUAL(expected, result);
 }
 
 CTEST(calc_less0_days, calc)
 {
-    int result = Dohod(22222, -5);
+    void result = Dohod(22222, -5);
 
-    const int expected = 0;
+    const void expected = 0;
 
     ASSERT_EQUAL(expected, result);
 }
 
 CTEST(calc_more365_days, calc)
 {
-    int result = Dohod(22222, 366);
+    void result = Dohod(22222, 366);
 
-    const int expected = 0;
+    const void expected = 0;
 
     ASSERT_EQUAL(expected, result);
 }
 /////////////////////////////////
 CTEST(calc_31_day_less_100000_rub, calc)
 {
-    int result = Dohod(50000, 31);
-    const int exp = 51000;
+    void result = Dohod(50000, 31);
+    const void exp = 51000;
 
     ASSERT_EQUAL(exp, result);
 }
 
 CTEST(calc_31_day_more_100000_rub, calc)
 {
-    int result = Dohod(150000, 31);
-    const int exp = 154500;
+    void result = Dohod(150000, 31);
+    const void exp = 154500;
 
     ASSERT_EQUAL(exp, result);
 }
 /////////////////////////////////
 CTEST(calc_120_day_less_100000_rub, calc)
 {
-    int result = Dohod(50000, 31);
-    const int exp = 51000;
+    void result = Dohod(50000, 31);
+    const void exp = 51000;
 
     ASSERT_EQUAL(exp, result);
 }
 
 CTEST(calc_120_day_more_100000_rub, calc)
 {
-    int result = Dohod(150000, 31);
-    const int exp = 154500;
+    void result = Dohod(150000, 31);
+    const void exp = 154500;
 
     ASSERT_EQUAL(exp, result);
 }
 /////////////////////////////////
 CTEST(calc_121_day_less_100000_rub, calc)
 {
-    int result = Dohod(50000, 121);
-    const int exp = 53000;
+    void result = Dohod(50000, 121);
+    const void exp = 53000;
 
     ASSERT_EQUAL(exp, result);
 }
 
 CTEST(calc_121_day_more_100000_rub, calc)
 {
-    int result = Dohod(150000, 121);
-    const int exp = 162000;
+    void result = Dohod(150000, 121);
+    const void exp = 162000;
 
     ASSERT_EQUAL(exp, result);
 }
 /////////////////////////////////
 CTEST(calc_240_day_less_100000_rub, calc)
 {
-    int result = Dohod(50000, 121);
-    const int exp = 53000;
+    void result = Dohod(50000, 121);
+    const void exp = 53000;
 
     ASSERT_EQUAL(exp, result);
 }
 
 CTEST(calc_240_day_more_100000_rub, calc)
 {
-    int result = Dohod(150000, 121);
-    const int exp = 162000;
+    void result = Dohod(150000, 121);
+    const void exp = 162000;
 
     ASSERT_EQUAL(exp, result);
 }
 /////////////////////////////////
 CTEST(calc_241_day_less_100000_rub, calc)
 {
-    int result = Dohod(50000, 241);
-    const int exp = 56000;
+    void result = Dohod(50000, 241);
+    const void exp = 56000;
 
     ASSERT_EQUAL(exp, result);
 }
 
 CTEST(calc_241_day_more_100000_rub, calc)
 {
-    int result = Dohod(150000, 241);
-    const int exp = 172500;
+    void result = Dohod(150000, 241);
+    const void exp = 172500;
 
     ASSERT_EQUAL(exp, result);
 }
@@ -118,16 +118,16 @@ CTEST(calc_241_day_more_100000_rub, calc)
 
 CTEST(calc_365_day_less_100000_rub, calc)
 {
-    int result = Dohod(50000, 241);
-    const int exp = 56000;
+    void result = Dohod(50000, 241);
+    const void exp = 56000;
 
     ASSERT_EQUAL(exp, result);
 }
 
 CTEST(calc_365_day_more_100000_rub, calc)
 {
-    int result = Dohod(150000, 241);
-    const int exp = 172500;
+    void result = Dohod(150000, 241);
+    const void exp = 172500;
 
     ASSERT_EQUAL(exp, result);
 }
